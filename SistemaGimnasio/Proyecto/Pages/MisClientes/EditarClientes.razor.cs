b57e3f0853245554ec1946fc139aaa99ad132e03
KevinFMO <fernandomatamoros963@gmail.com>
2022-08-09 15:32:41 -0600

    clientes
    clientes

---
+++ SistemaGimnasio/Proyecto/Pages/MisClientes/EditarClientes.razor.cs
@@ -0,0 +1,82 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+using Proyecto.Interfaces;
+
+namespace Proyecto.Pages.MisClientes
+{
+    partial class EditarClientes
+    {
+        [Inject] IClienteServicio clienteServicio { get; set; }
+        [Inject] NavigationManager navigationManager { get; set; }
+        [Parameter] public string Identidad { get; set; }
+        [Inject] SweetAlertService Swal { get; set; }
+
+        private Cliente cliente = new Cliente();
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (!string.IsNullOrEmpty(Identidad))
+            {
+                cliente = await clienteServicio.GetPorIdentidad(Identidad);
+            }
+        }
+
+        protected async void Guardar()
+        {
+            if (string.IsNullOrEmpty(cliente.Identidad) || string.IsNullOrEmpty(cliente.Nombre))
+            {
+                return;
+            }
+
+            bool edito = await clienteServicio.Actualizar(cliente);
+
+            if (edito)
+            {
+                await Swal.FireAsync("Guardar", "Cliente actualizado con exito", SweetAlertIcon.Success);
+            }
+            else
+            {
+                await Swal.FireAsync("Error", "No se pudo actualizar el cliente", SweetAlertIcon.Error);
+            }
+            navigationManager.NavigateTo("/Clientes");
+        }
+
+        protected void Cancelar()
+        {
+            navigationManager.NavigateTo("/Clientes");
+        }
+
+
+        protected async Task Eliminar()
+        {
+            bool elimino = false;
+
+            SweetAlertResult result = await Swal.FireAsync(new SweetAlertOptions
+            {
+                Title = "¿Seguro que desea eliminar el cliente?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true,
+                ConfirmButtonText = "Si",
+                CancelButtonText = "NO"
+            });
+
+            if (!string.IsNullOrEmpty(result.Value))
+            {
+                elimino = await clienteServicio.Eliminar(cliente);
+                if (elimino)
+                {
+                    await Swal.FireAsync("Atención", "Cliente eliminado con exito", SweetAlertIcon.Success);
+                    navigationManager.NavigateTo("/Clientes");
+                }
+                else
+                {
+                    await Swal.FireAsync("Error", "No se pudo eliminar el cliente", SweetAlertIcon.Error);
+                }
+            }
+
+
+        }
+
+    }
+}   
---
+++ SistemaGimnasio/Proyecto/Pages/MisClientes/NuevoCliente.razor.cs
@@ -0,0 +1,48 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+using Proyecto.Interfaces;
+
+namespace Proyecto.Pages.MisClientes
+{
+    partial class NuevoCliente
+    {
+        [Inject] private IClienteServicio clienteServicio { get; set; }
+        [Inject] SweetAlertService Swal { get; set; }
+        [Inject] NavigationManager _navigationManager { get; set; }
+
+        private Cliente cl = new Cliente();
+
+
+        //protected override async Task OnInitializedAsync()
+        //{
+           
+        //}
+
+        protected async void Guardar()
+        {
+            if (string.IsNullOrEmpty(cl.Identidad) || string.IsNullOrEmpty(cl.Nombre) )
+            {
+                return;
+            }
+
+            bool inserto = await clienteServicio.Nuevo(cl);
+
+            if (inserto)
+            {
+                await Swal.FireAsync("Guardar", "Cliente guardado con exito", SweetAlertIcon.Success);
+            }
+            else
+            {
+                await Swal.FireAsync("Error", "No se pudo guardar el cliente", SweetAlertIcon.Error);
+            }
+            _navigationManager.NavigateTo("/Clientes");
+        }
+
+        protected void Cancelar()
+        {
+            _navigationManager.NavigateTo("/Clientes");
+        }
+
+    }
+}
